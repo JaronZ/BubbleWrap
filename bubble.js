5bd8bb2222d2306c8508ae0cc9c1e0f34104f83a
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-02-13 16:07:17 +0100

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -2,7 +2,7 @@ const { Client, RichEmbed } = require('discord.js');
 const client = new Client();
 const { token } = require('./config.json');
 var messageAmount = 0;
-var goal = Math.floor((Math.random() * 20) + 1);
+var goal = 12;
 
 client.once('ready', () => {
     console.log('bot is ready!');
@@ -41,7 +41,5 @@ client.on('message', message => {
     if (messageAmount == goal) {
         message.channel.send("||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop||");
         messageAmount = 0;
-        goal = Math.floor((Math.random() * 20) + 1);
-        console.log("new message goal: " + goal);
     }
 });
