6d72a8d7d6176891d5f5c304c6828cf6eb9a9150
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-02-13 18:39:56 +0100

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -3,6 +3,7 @@ const client = new Client();
 const { token } = require('./config.json');
 var messageAmount = 0;
 var goal = 200;
+var msg = "||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop||";
 
 client.once('ready', () => {
     console.log('bot is ready!');
@@ -20,16 +21,18 @@ process.on('unhandledRejection', error => {
 
 client.on('message', message => {
     if (message.author.bot) return;
-
-    messageAmount++;
     
-    if (message.content == 'bubble goal') {
-        const embedMsg = new RichEmbed()
+    const embedMsg = new RichEmbed()
             .setColor('#0000ff')
             .setAuthor(message.author.username, message.author.displayAvatarURL)
             .setThumbnail(message.author.displayAvatarURL)
             .setTimestamp()
-            .setFooter(client.user.username, client.user.displayAvatarURL)
+            .setFooter(client.user.username, client.user.displayAvatarURL);
+
+    messageAmount++;
+    
+    if (message.content == 'bubble goal') {
+        embedMsg
             .addField('Message Goal', goal, true)
             .addField('Sent Messages', messageAmount, true);
 
@@ -37,13 +40,29 @@ client.on('message', message => {
     }
     
     if (message.content.startsWith('bubble send')) {
+        var user = message.mentions.users.first();
+        if (!user){
+            embedMsg
+                .setDescription('Please mention a user! The proper usage is bubble send <mention>')
+                .setColor('#ff0000');
             
+            return message.channel.send(embedMsg);
+        }
+        return user.send(msg).then(() => {
+            embedMsg.setDescription('message sent');
+            message.channel.send(embedMsg);
+        }).catch((err) => {
+            embedMsg
+                .setDescription('could not send message to ' + user)
+                .setColor('#ff0000');
+            message.channel.send(embedMsg);
+        });
     }
 
     console.log("message amount: " + messageAmount);
     console.log("message goal: " + goal);
     if (messageAmount == goal) {
-        message.channel.send("||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop|| ||pop||");
+        message.channel.send(msg);
         messageAmount = 0;
     }
 });
